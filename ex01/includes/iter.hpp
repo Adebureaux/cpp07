bb3f92be75003f05c0f844655a92c3478c919655
Augustin Debureaux <adebureaux@gmail.com>
2022-05-12 23:44:23 +0200

    save

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -5,5 +5,10 @@
 #include <string>
 
 template <typename T>
+void iter(T array[], size_t size, void (*f)(T &))
+{
+	for (size_t i = 0; i < size; i++)
+		f(array[i]);
+}
 
 #endif
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,7 +1,30 @@
 #include "../includes/iter.hpp"
 
+void divide_int(int & n)
+{
+	n /= 10;
+}
+
+void divide_double(double & d)
+{
+	d /= 10;
+}
+
 int main(void)
 {
-	
+	{
+		int a[5] = { 10, 50, 30, 40, 20 };
+
+		std::cout << a[0] << std::endl;
+		iter<int>(a, 5, &divide_int);
+		std::cout << a[0] << std::endl;
+	}
+	{
+		double a[5] = { 25.5, 31.0, 12, 14, 17 };
+
+		std::cout << a[0] << std::endl;
+		iter<double>(a, 5, &divide_double);
+		std::cout << a[0] << std::endl;
+	}
 	return (0);	
 }
